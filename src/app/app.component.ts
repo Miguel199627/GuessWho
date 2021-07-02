5b819d1320536361518e0d376465198a838d52e1
miguel199627 <cerquera199627@hotmail.com>
2021-07-02 11:24:31 -0500

    Maquetacion html y creacion de personas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GuessWho';
+  public personas: any = [
+    {
+      nombre: 'Miguel Cerquera',
+      sexo: false,
+      barba: false,
+      ropaClara: false,
+      colorPelo: "rubio",
+      tipoPelo: "liso",
+      foto: "http://localhost:4200/assets/miguel.png"
+    },
+    {
+      nombre: 'Edson Suarez',
+      sexo: false,
+      barba: true,
+      ropaClara: false,
+      colorPelo: "negro",
+      tipoPelo: "ondulado",
+      foto: "http://localhost:4200/assets/edson.jpg"
+    },
+    {
+      nombre: 'Natalia Ortiz',
+      sexo: true,
+      barba: false,
+      ropaClara: true,
+      colorPelo: "castaño",
+      tipoPelo: "ondulado",
+      foto: "http://localhost:4200/assets/natalia.jpg"
+    },
+    {
+      nombre: 'Cristian Gomez',
+      sexo: false,
+      barba: true,
+      ropaClara: false,
+      colorPelo: "negro",
+      tipoPelo: "crespo",
+      foto: "http://localhost:4200/assets/cristian.jpg"
+    },
+    {
+      nombre: 'Stiven Quiroga',
+      sexo: false,
+      barba: false,
+      ropaClara: true,
+      colorPelo: "castaño",
+      tipoPelo: "liso",
+      foto: "http://localhost:4200/assets/stiven.jpg"
+    }
+  ];
+
+  constructor() {}
 }
