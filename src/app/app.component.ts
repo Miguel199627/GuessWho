b78234f34e9d56a8a4a3332a8f3f6db42020c2d8
miguel199627 <cerquera199627@hotmail.com>
2021-07-02 12:03:51 -0500

    Add question at game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
       sexo: false,
       barba: false,
       ropaClara: false,
+      select: false,
       colorPelo: "rubio",
       tipoPelo: "liso",
       foto: "http://localhost:4200/assets/miguel.png"
@@ -22,6 +23,7 @@ export class AppComponent {
       sexo: false,
       barba: true,
       ropaClara: false,
+      select: false,
       colorPelo: "negro",
       tipoPelo: "ondulado",
       foto: "http://localhost:4200/assets/edson.jpg"
@@ -31,6 +33,7 @@ export class AppComponent {
       sexo: true,
       barba: false,
       ropaClara: true,
+      select: false,
       colorPelo: "castaño",
       tipoPelo: "ondulado",
       foto: "http://localhost:4200/assets/natalia.jpg"
@@ -40,6 +43,7 @@ export class AppComponent {
       sexo: false,
       barba: true,
       ropaClara: false,
+      select: false,
       colorPelo: "negro",
       tipoPelo: "crespo",
       foto: "http://localhost:4200/assets/cristian.jpg"
@@ -49,11 +53,49 @@ export class AppComponent {
       sexo: false,
       barba: false,
       ropaClara: true,
+      select: false,
       colorPelo: "castaño",
       tipoPelo: "liso",
       foto: "http://localhost:4200/assets/stiven.jpg"
     }
   ];
+  public personSelectUser: any;
+  public personSelectMach: any;
+  public hideSelect: Boolean;
+  public preguntas: any = [
+    "¿Es hombre?",
+    "¿Es mujer?",
+    "¿Tiene anteojos?",
+    "¿Tiene pelo rubio?",
+    "¿Tiene pelo castaño?",
+    "¿Tiene pelo negro?",
+    "¿Tiene barba?",
+    "¿Tiene pelo crespo?",
+    "¿Tiene pelo lizo?",
+    "¿Tiene pelo ondulado?",
+    "¿Tiene ropa clara?",
+    "¿Tiene ropa oscura?"
+  ];
 
-  constructor() {}
+  constructor() {
+    this.hideSelect = true;
+  }
+
+  select(item: any) {
+    this.personas.forEach( (data: any) => {
+      data.select = false;
+    });
+    item.select = true;
+    this.personSelectUser = item;
+    this.hideSelect = false;
+    const number = this.getRandomArbitrary(0, 4);
+    this.personSelectMach = this.personas[number];
+    const numberQuestion = this.getRandomArbitrary(0, 11);
+    const preguntaMach = this.preguntas[numberQuestion];
+    console.log(preguntaMach);
+  }
+
+  getRandomArbitrary(min: number, max: number) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
 }
